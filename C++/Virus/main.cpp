d031050a2b41937ad1cc3f7327337e43e64f8265
inseonyun <yuninseon@naver.com>
2022-01-18 17:17:21 +0900

    백준 바이러스 문제풀이 코드 수정

--- C++/Virus/main.cpp
+++ C++/Virus/main.cpp
@@ -5,41 +5,41 @@
 
 #include <iostream>
 #include <cstring>
-#include <algorithm>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
-vector <int> v[101];
-queue <int> q;
-bool visited[101];
-int N, M;
-int virusN = 0;
+vector<int> v[101];
+queue<int> q;
+bool check[101];
+int virus_cnt = 0;
 
 void DFS(int x) {
-	visited[x] = true;
+	check[x] = true;
+
 	for (int i = 0; i < v[x].size(); i++) {
 		int y = v[x][i];
-		if (!visited[y]) {
-			virusN++;
+		if (!check[y]) {
+			virus_cnt++;
 			DFS(y);
 		}
 	}
 }
 
 void BFS() {
-	visited[1] = true;
-
+	check[1] = true;
 	q.push(1);
+
 	while (!q.empty()) {
 		int x = q.front();
+
 		q.pop();
+
 		for (int i = 0; i < v[x].size(); i++) {
 			int y = v[x][i];
-			if (visited[y] == false) {
-				visited[y] = true;
-				virusN++;
+			if (!check[y]) {
+				check[y] = true;
+				virus_cnt++;
 				q.push(y);
 			}
 		}
@@ -47,24 +47,22 @@ void BFS() {
 }
 
 int main() {
-	cin >> N;
-	cin >> M;
+	int N, M;
+	cin >> N >> M;
+
+	memset(check, false, sizeof(check));
 
 	for (int i = 0; i < M; i++) {
 		int a, b;
 		cin >> a >> b;
+
 		v[a].push_back(b);
 		v[b].push_back(a);
-
 	}
-	//memset(visited, false, sizeof(visited));
-	//DFS(1);
+	DFS(1);
+	//BFS();
 
-	//virusN = 0;
-	memset(visited, false, sizeof(visited));
-	BFS();
-
-	cout << virusN << "\n";
+	cout << virus_cnt << "\n";
 
 	return 0;
 }
