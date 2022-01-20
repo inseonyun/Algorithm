8fc8e2f0363bb9563e0ad47a567d8361bc403e9f
inseonyun <yuninseon@naver.com>
2022-01-20 14:06:37 +0900

    백준 균형잡힌 세상 문제풀이 추가

---
+++ C++/BalanceWorld/main.cpp
@@ -0,0 +1,53 @@
+
+//////////////////////////////////////////////////
+// BAEKJOON_4909¹ø: ±ÕÇüÀâÈù ¼¼»ó
+//////////////////////////////////////////////////
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	while (true) {
+		vector <char> openChar;
+		string str;
+
+		getline(cin, str);
+
+		if (str[0] == '.')
+			break;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] == '(' || str[i] == '[') {
+				openChar.push_back(str[i]);
+			}
+			if (str[i] == ')') {
+				if (!openChar.empty() && openChar.back() == '(') {
+					openChar.pop_back();
+				}
+				else {
+					cout << "no\n";
+					break;
+				}
+			}
+			if (str[i] == ']') {
+				if (!openChar.empty() && openChar.back() == '[') {
+					openChar.pop_back();
+				}
+				else {
+					cout << "no\n";
+					break;
+				}
+			}
+			if (openChar.empty() && i == str.length() - 2)
+				cout << "yes\n";
+			else if (!openChar.empty() && i == str.length() - 2)
+				cout << "no\n";
+		}
+	}
+
+	return 0;
+}
