49808a37f45e4992d250462f3305e9ff85f1090d
inseonyun <yuninseon@naver.com>
2022-03-25 11:40:12 +0900

    Batch Files 삭제

--- batch files/checkout.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd %git_file_path%
-
-git pull
-git checkout -b %newBranchName%
-
-echo Create New Branch : "%newBranchName%"
--- batch files/pull&delete.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd %git_file_path%
-git checkout master
-git pull upstream master
-git branch -D %newBranchName%
-git push origin :%newBranchName%
-git push
-
-echo Pull And Delete "%newBranchName%"
